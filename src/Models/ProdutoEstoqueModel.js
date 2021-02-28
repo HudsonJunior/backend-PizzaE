a4febae7a282eefafe06b32ea76e61a8cd6b0872
Edson Cizeski <cizeskiedson@gmail.com>
2021-02-28 15:22:04 -0300

    Service atualizado.

--- src/Models/ProdutoEstoqueModel.js
+++ src/Models/ProdutoEstoqueModel.js
@@ -1,18 +1,12 @@
-
 class ProdutoEstoqueModel {
 
     constructor(data) {
-        this.codigo = data.codigo
+        this.id = data.id
         this.nome = data.nome
-        this.marca = data.marca
         this.quantidade = data.quantidade
-        this.quantidade_minima = data.quantidade_minima
-        this.valor_item= data.valor_item
-        this.peso_item = data.peso_item
-        this.data_validade = data.data_validade
-        this.data_fabricacao = data.data_fabricacao
-        this.data_registro = data.data_registro
-        this.data_promo = data.data_promo
+        this.marca = data.marca
+        this.itens = data.itens
+        this.quantidadeMinima = data.quantidadeMinima
     }
 }
 module.exports = ProdutoEstoqueModel
--- src/Services/ProdutosEstoqueService.js
+++ src/Services/ProdutosEstoqueService.js
@@ -1,36 +1,50 @@
 /* imports */
 
 const string = require('./../Common/String')
-
+const ProdutosEstoqueDao = require('./../Daos/ProdutosEstoqueDao')
 /* Global variables*/
 
 var produtosEstoqueService
-
+var produtosEstoqueDao
 /* */
 
 class ProdutosEstoqueService {
     constructor() {
         produtosEstoqueService = this
+        produtosEstoqueDao = new ProdutosEstoqueDao()
+
     }
 
-    async create(UserModel) {
+    async create(EstoqueModel) {
         return new Promise(async function (resolve, reject) {
             try {
+                let produtoCadastrado = false
 
-                userDal.create(UserModel)
+
+                produtosEstoqueDao.findOne(EstoqueModel)
                     .then(result => {
-                        resolve(result)
+                        produtoCadastrado = true;
+                        reject(Exceptions.generateException(400, "Produto com mesmo nome ou código já cadastrado", "Não é possivel cadastrar um produto com mesmo código ou nome"))
                     })
                     .catch(error => {
                         reject(error)
-                    })
-
+                    });
+                if (!produtoCadastrado) {
+                    produtosEstoqueDao.create(UserModel)
+                        .then(result => {
+                            resolve(result)
+                        })
+                        .catch(error => {
+                            reject(error)
+                        })
+                }
             }
             catch (error) {
                 reject(error)
             }
         })
     }
+
 
 
 }
