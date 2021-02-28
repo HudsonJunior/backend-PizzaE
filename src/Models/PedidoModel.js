a20f2b6a38f1706524baa71b33831012b2fad958
Michely <michely@inovaclick.com.br>
2021-02-28 15:24:50 -0300

    adicao de cpfCliente no pedidoModel

--- src/Models/PedidoModel.js
+++ src/Models/PedidoModel.js
@@ -3,11 +3,11 @@ class Pedido {
 
     constructor(data) {
         this.produtos = data.produtos // lista de produtos: {codigo:"" quantidade:""}
-        this.codigo = data.codigo
         this.formaPagamento = data.formaPagamento
         this.formaExpedicao = data.formaExpedicao
         this.data = data.data
-        this.cpf = data.cpf
+        this.cpfCliente = data.cpfCliente
+        this.cpfNF = data.cpfNF
         this.observacoes = data.observacoes
         this.status = data.status
     }
